257fea069c2b3cf167418299a7b2313b6090bfe5
Niklan <niklanrus@gmail.com>
2018-01-25 13:24:44 +0500

    - Removed console.log() from JS. - A bit changed date formatting using Drupal service. - Added new settings: formatDate, formatTime, minDate, maxDate, minTime, maxTime. - Added description to settings form with link to documentation.

--- src/Plugin/Field/FieldWidget/DateTimePickerWidget.php
+++ src/Plugin/Field/FieldWidget/DateTimePickerWidget.php
@@ -29,6 +29,12 @@ class DateTimePickerWidget extends WidgetBase {
         'inline' => 0,
         'allow_times' => '',
         'mask' => '',
+        'format_date' => NULL,
+        'format_time' => NULL,
+        'min_date' => NULL,
+        'max_date' => NULL,
+        'min_time' => NULL,
+        'max_time' => NULL,
       ] + parent::defaultSettings();
   }
 
@@ -37,9 +43,15 @@ class DateTimePickerWidget extends WidgetBase {
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     $default_value = isset($items[$delta]->value) ? $items[$delta]->value : '';
-    $default_value_corrected = date($this->getSetting('format'), strtotime($default_value));
+    $default_value_corrected = \Drupal::service('date.formatter')
+      ->format(strtotime($default_value), 'custom', $this->getSetting('format'));
 
     $settings = $this->getSettings();
+    // Why is we doing this manual conditions below? This is all because some
+    // settings must be transformed or checked properly before they will be send
+    // to JS. F.e. we need transform string to array for allowTimes or check
+    // is value is set for min_date, where is 0 is also valid value and we can't
+    // pass it trough empty().
     // Mask
     if (empty($settings['mask'])) {
       $settings['mask'] = FALSE;
@@ -50,6 +62,30 @@ class DateTimePickerWidget extends WidgetBase {
     // Allowed times.
     if (!empty($settings['allow_times'])) {
       $settings['allow_times'] = explode(',', $settings['allow_times']);
+    }
+    // Format date.
+    if (strlen($settings['format_date']) == 0) {
+      unset($settings['format_date']);
+    }
+    // Format time.
+    if (strlen($settings['format_time']) == 0) {
+      unset($settings['format_time']);
+    }
+    // Min date.
+    if (strlen($settings['min_date']) == 0) {
+      unset($settings['min_date']);
+    }
+    // Max date.
+    if (strlen($settings['max_date']) == 0) {
+      unset($settings['max_date']);
+    }
+    // Min time.
+    if (strlen($settings['min_time']) == 0) {
+      unset($settings['min_time']);
+    }
+    // Max time.
+    if (strlen($settings['max_time']) == 0) {
+      unset($settings['max_time']);
     }
 
     // Convert all settings array keys to camelCase.
@@ -97,6 +133,14 @@ class DateTimePickerWidget extends WidgetBase {
     $element = parent::settingsForm($form, $form_state);
 
     if ($this->getFieldSetting('datetime_type') == 'datetime') {
+      $element['help'] = [
+        '#markup' => $this->t('This settings are represents actual settings for library. For more information and examples refer to <a href="@href" target="_blank">documentation</a>.', [
+          '@href' => 'https://xdsoft.net/jqplugins/datetimepicker/',
+        ]),
+        '#prefix' => '<p>',
+        '#suffix' => '</p>',
+      ];
+
       $element['format'] = [
         '#type' => 'textfield',
         '#title' => $this->t('Format'),
@@ -123,6 +167,44 @@ class DateTimePickerWidget extends WidgetBase {
         '#title' => $this->t('Mask'),
         '#description' => $this->t('Enable mask support for field. Leave it empty to leave disabled, enter \'auto\' to enable automatically mask, or enter exact mask which you want.'),
         '#default_value' => $this->getSetting('mask'),
+      ];
+
+      $element['format_date'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Format date'),
+        '#description' => $this->t('Format date for minDate and maxDate. Default: Y/m/d.'),
+        '#default_value' => $this->getSetting('format_date'),
+      ];
+
+      $element['format_time'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Format time'),
+        '#description' => $this->t('Similarly, formatDate. But for minTime and maxTime. Default: H:i.'),
+        '#default_value' => $this->getSetting('format_time'),
+      ];
+
+      $element['min_date'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Minimal date'),
+        '#default_value' => $this->getSetting('min_date'),
+      ];
+
+      $element['max_date'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Maximum date'),
+        '#default_value' => $this->getSetting('max_date'),
+      ];
+
+      $element['min_time'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Minimum time'),
+        '#default_value' => $this->getSetting('min_time'),
+      ];
+
+      $element['max_time'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Maximum time'),
+        '#default_value' => $this->getSetting('max_time'),
       ];
     }
 
